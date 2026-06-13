11cb2cb18f992dd9c6669cfac446698aed0ef96d
Sharavana Priyan S S <sharavanapriyanss2007@gmail.com>
2026-06-13 21:59:03 +0530

    Create sum_using_pointers.c

---
+++ 5. Pointers/sum_using_pointers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+    int *p1, *p2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    p1 = &a;
+    p2 = &b;
+
+    printf("Sum = %d", *p1 + *p2);
+
+    return 0;
+}
