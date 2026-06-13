a683c72b2931421b89c01660cf3895a6afc2aa58
Sharavana Priyan S S <sharavanapriyanss2007@gmail.com>
2026-06-13 21:59:21 +0530

    Create array_using_pointers.c

---
+++ 5. Pointers/array_using_pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *ptr = arr;
+
+    printf("Array Elements:\n");
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", *(ptr + i));
+    }
+
+    return 0;
+}
