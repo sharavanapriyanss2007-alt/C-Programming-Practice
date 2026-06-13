1958397c59c5f5f66c3d13255b5cd01221c38a44
Sharavana Priyan S S <sharavanapriyanss2007@gmail.com>
2026-06-13 21:00:42 +0530

    Create string_length.c

---
+++ 3. Strings/string_length.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Length = %d", strlen(str) - 1);
+
+    return 0;
+}
