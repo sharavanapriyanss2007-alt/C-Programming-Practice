0505a85ad5de118b48efa836140dda7736269008
Sharavana Priyan S S <sharavanapriyanss2007@gmail.com>
2026-06-13 21:01:16 +0530

    Create palindrome_string.c

---
+++ 3. Strings/palindrome_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int flag = 1;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    for(int i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - i - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
